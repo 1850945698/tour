ba4703772d775824682411aa84463af7b2f6609c
LeeZhongSukzeng <2445814729@qq.com>
2019-01-08 08:55:47 +0800

    详情

--- js/details.js
+++ js/details.js
@@ -1,3 +1,42 @@
 window.onload = function(){
-	
+	var bannerList = [
+		{
+			"linksrc":"http://sina.com",
+			"imgsrc": "img/images/1.jpg"
+		},
+		{
+			"linksrc":"http://sina.com",
+			"imgsrc": "img/images/2.jpg"
+		},
+		{
+			"linksrc":"http://sina.com",
+			"imgsrc": "img/images/3.jpg"
+		},
+		{
+			"linksrc":"http://sina.com",
+			"imgsrc": "img/images/4.jpg"
+		}
+	]
+
+	var index = 0;	
+	var links = document.getElementById("links");
+	var bannerimg = document.getElementById("bannerimg");	
+	function changeImg() {
+		if(index == 3){
+			index = 0;
+		}
+		else {
+			index = index + 1;
+		}
+		links.href = bannerList[index].linksrc;
+		bannerimg.src = bannerList[index].imgsrc;
+	}
+	var intervalId = setInterval(changeImg,1000)
+
+	bannerimg.onmouseover = function() {
+		clearInterval(intervalId);
+	}
+	bannerimg.onmouseout = function() {
+		intervalId = setInterval(changeImg, 1000);
+	}
 }
