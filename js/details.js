5e949311e3dadeea4ef9b56a9d0ecd8e14e4baf1
LeeZhongSukzeng <2445814729@qq.com>
2019-01-08 08:58:39 +0800

    详情

--- js/details.js
+++ js/details.js
@@ -1,25 +1,20 @@
 window.onload = function(){
 	var bannerList = [
 		{
-			"linksrc":"http://sina.com",
 			"imgsrc": "img/images/1.jpg"
 		},
 		{
-			"linksrc":"http://sina.com",
 			"imgsrc": "img/images/2.jpg"
 		},
 		{
-			"linksrc":"http://sina.com",
 			"imgsrc": "img/images/3.jpg"
 		},
 		{
-			"linksrc":"http://sina.com",
 			"imgsrc": "img/images/4.jpg"
 		}
 	]
 
 	var index = 0;	
-	var links = document.getElementById("links");
 	var bannerimg = document.getElementById("bannerimg");	
 	function changeImg() {
 		if(index == 3){
@@ -28,7 +23,6 @@ window.onload = function(){
 		else {
 			index = index + 1;
 		}
-		links.href = bannerList[index].linksrc;
 		bannerimg.src = bannerList[index].imgsrc;
 	}
 	var intervalId = setInterval(changeImg,1000)
