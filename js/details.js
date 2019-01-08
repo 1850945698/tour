9644da951f5fd82a4e9c628e0987038ef8bbbd0f
LeeZhongSukzeng <2445814729@qq.com>
2019-01-08 11:25:32 +0800

    详情

--- js/details.js
+++ js/details.js
@@ -1,4 +1,18 @@
 window.onload = function(){
+	var ulList = document.getElementById("ullist");
+	var liList = ulList.getElementsByTagName("li");
+	for(var a=0; a<liList.length; a++){
+		liList[a].onmouseover = function(){
+			this.className = "now";
+			
+		}
+		liList[a].onmouseout = function(){
+			this.className = "";
+			liList[1].className = "isnow";
+			
+		}
+		
+	}
 	/*轮播*/
 	var bannerList = [
 		{
